e841b0cad5e1d48ff38ea1d08b3473ff631ed6b0
MigaraArcher <evamarcod@gmail.com>
2019-04-11 17:10:25 +0200

    Carrera de tortugas operativa

--- Carrera_de_tortugas.py
+++ Carrera_de_tortugas.py
@@ -1,10 +1,12 @@
 import turtle
+import random
 
 
-class Circuito():
+class Circuito:
     corredores = []
     __posstarty = (-30, -10, 10, 30)
     __colorturtle = ('red', 'Blue', 'green', 'orange')
+    # Inicia la pantalla
 
     def __init__(self, width, height):
         self.__screen = turtle.Screen()
@@ -12,6 +14,10 @@ class Circuito():
         self.__screen.bgcolor('lightgrey')
         self.__starline = (-width/2 + 5 * width/100)
         self.__endline = (width/2 + 5 * width/100)
+        self.__createrunners()
+        
+    # Creamos los corredores.
+    def __createrunners(self):
         for i in range(4):
             new_turtle = turtle.Turtle()
             new_turtle.color(self.__colorturtle[i])
@@ -20,7 +26,20 @@ class Circuito():
             new_turtle.setpos(self.__starline, self.__posstarty[i])
 
             self.corredores.append(new_turtle)
+        
+    def competir(self):
+        hayganador = False
+        while not hayganador:
+            for tortu in self.corredores:
+                avanza = random.randint(1, 6)
+                tortu.forward(avanza)
+                if tortu.position()[0] >= self.__endline:
+                    hayganador = True
+                    print('!!!!La tortuga de color {} ha ganado¡¡¡¡'.format(tortu.color()[0]))
+                    break
 
 
 if __name__ == '__main__':
     circuito = Circuito(400, 400)
+    circuito.competir()
+
